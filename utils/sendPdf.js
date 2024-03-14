ee65db9730c6b8899f38e8030d673b5f5513b625
Rachit Patel <rachitp509@gmail.com>
2024-03-15 00:33:17 +0530

    bug fixes

--- utils/sendPdf.js
+++ utils/sendPdf.js
@@ -1,7 +1,7 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs").promises;
 const os = require("os");
-const { join } = require("path");
+const path = require("path");
 
 exports.sendInvoice = async (product) => {
   let alltotal = 0;
@@ -14,6 +14,16 @@ exports.sendInvoice = async (product) => {
   grandtotal = alltotal + (alltotal * 18) / 100;
 
   try {
+    const userDataDir = await fs.mkdtemp(
+      path.join(os.tmpdir(), "puppeteer_temp")
+    );
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      userDataDir, // Specify a temporary directory for user data
+    });
+
+    const page = await browser.newPage();
+
     const htmlTemplate = `<div
       style="
         display: flex;
@@ -37,79 +47,90 @@ exports.sendInvoice = async (product) => {
           <span>infotech</span>
         </div>
       </div>
-      <table
+      <div
         style="
           font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI',
             Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
             sans-serif;
-          width: 100%;
-          border-collapse: collapse;
         "
       >
-        <thead>
-          <tr>
-            <th style="font-weight: bold; border-bottom: 1px solid #ebebeb;">Product Name</th>
-            <th style="font-weight: bold; border-bottom: 1px solid #ebebeb;">Qty</th>
-            <th style="font-weight: bold; border-bottom: 1px solid #ebebeb;">Rate</th>
-            <th style="font-weight: bold; border-bottom: 1px solid #ebebeb;">Total</th>
-          </tr>
-        </thead>
-        <tbody>
-          ${product
-            .map(
-              (item, index) => `
-            <tr key=${index} style="border-bottom: 1px solid #ebebeb;">
-              <td>${item.name}</td>
-              <td style="color: #7f88bd;">${item.qty}</td>
-              <td>${item.rate}</td>
-              <td>INR ${alltotal}</td>
-            </tr>
-          `
-            )
-            .join("")}
-        </tbody>
-      </table>
-      <div style="border-top: 2px solid #ebebeb; margin-top: 1.5rem">
-        <div
+        <ul
           style="
+            padding-left: 0;
             display: flex;
-            float: right;
-            flex-direction: column;
-            gap: 1rem;
-            width: 40%;
-            margin-top: 1rem;
+            justify-content: space-between;
+            border-bottom: 1px solid #ebebeb;
+            padding-bottom: 1rem;
           "
         >
+          <li style="font-weight: bold; list-style: none">Product Name</li>
+          <li style="font-weight: bold; list-style: none">Qty</li>
+          <li style="font-weight: bold; list-style: none">Rate</li>
+          <li style="font-weight: bold; list-style: none">Total</li>
+        </ul>
+        ${product
+          .map(
+            (item, index) => `
+          <ul
+            key=${index}
+            style="
+              padding-left: 0;
+              display: flex;
+              justify-content: space-between;
+              font-size: 14px;
+              margin-bottom: 2rem;
+            "
+          >
+            <li style="list-style: none">${item.name}</li>
+            <li style="list-style: none; color: #7f88bd">${item.qty}</li>
+            <li style="list-style: none">${item.rate}</li>
+            <li style="list-style: none">INR ${alltotal}</li>
+          </ul>
+        `
+          )
+          .join("")}
+        <div style="border-top: 2px solid #ebebeb; margin-top: 1.5rem">
           <div
             style="
               display: flex;
-              justify-content: space-between;
-              align-items: center;
+              float: right;
+              flex-direction: column;
+              gap: 1rem;
+              width: 40%;
+              margin-top: 1rem;
             "
           >
-            <span style="font-weight: bold">Total</span><span>INR ${alltotal}</span>
-          </div>
-          <div
-            style="
-              display: flex;
-              justify-content: space-between;
-              align-items: center;
-            "
-          >
-            <span>GST</span><span style="color: rgb(180, 180, 180)">18%</span>
-          </div>
-          <div
-            style="
-              display: flex;
-              justify-content: space-between;
-              align-items: center;
-              padding: 1rem 0;
-              border-bottom: 2px solid #ebebeb;
-              border-top: 2px solid #ebebeb;
-            "
-          >
-            <span style="font-weight: bold">Grand Total</span
-            ><span style="color: #7f88bd">₹ ${grandtotal}</span>
+            <div
+              style="
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+              "
+            >
+              <span style="font-weight: bold">Total</span><span>INR ${alltotal}</span>
+            </div>
+            <div
+              style="
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+              "
+            >
+              <span>GST</span><span style="color: rgb(180, 180, 180)">18%</span>
+            </div>
+            <div
+              style="
+                display: flex;
+                justify-content: space-between;
+                align-items: center;
+                padding: 1rem 0;
+                border-bottom: 2px solid #ebebeb;
+                border-top: 2px solid #ebebeb;
+              "
+            >
+              <span style="font-weight: bold">Grand Total</span
+              ><span style="color: #7f88bd">₹ ${grandtotal}</span>
+            </div>
           </div>
         </div>
       </div>
@@ -121,7 +142,7 @@ exports.sendInvoice = async (product) => {
           sans-serif;margin-top: 5rem;
       "
     >
-      <div style="font-size: 14px;padding-left: 2rem;margin-left:2rem;">
+      <div style="font-size: 14px;padding-left: 2rem;margin-bottom:1.5rem;">
         <span style="font-weight: bold">Valid Until</span>: 12/04/23
       </div>
       <div style="background-color: #2d2c2c; color: white; padding: 1px 3rem;border-radius: 50px;margin: 0 1rem;">
@@ -134,27 +155,17 @@ exports.sendInvoice = async (product) => {
           careful attention.
         </p>
       </div>
-    </footer>`;
+    </footer>`
 
-    const browser = await puppeteer.launch({
-      userDataDir: join(__dirname, ".cache", `puppeteer`, `${Date.now()}`),
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    });
-    const page = await browser.newPage();
     await page.setContent(htmlTemplate);
-
     const pdfBuffer = await page.pdf({
       format: "A4",
       printBackground: true,
     });
 
     await browser.close();
+    await fs.rm(userDataDir, { recursive: true });
 
-    // Remove the temporary user data directory
-    fs.rmdirSync(join(__dirname, ".cache", `puppeteer`, `${Date.now()}`), {
-      recursive: true,
-      // force: true,
-    });
     return pdfBuffer;
   } catch (error) {
     console.error("Error sending invoice:", error);
